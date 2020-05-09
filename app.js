d0694fbe6792cfd0267ff81684bdda027bb558f5
bohaochen <360485406@qq.com>
2020-05-10 00:33:38 +0800

    1

--- app.js
+++ app.js
@@ -2,46 +2,63 @@
 const http = require('http')
 const spawn = require('child_process').spawn
 const createHandler = require('github-webhook-handler')
+const express = require('express');
+const history = require(`connect-history-api-fallback`);
+const {createProxyMiddleware } = require("http-proxy-middleware");
+
+var apiPath = "/api/";//目标后端服务地址(反向代理指向地址)
+var proxyPath = "http://116.62.197.100:1337/";//目标后端服务地址(反向代理指向地址)
+var proxyOption ={
+  target:proxyPath,
+  changeOrigoin:true,
+  pathRewrite: function (path) { return path.replace(`${apiPath}`, '/') }
+};
+
+var app = express();
+
 const handler = createHandler({
   path: '/',
   secret: '123456'
 })
- 
+
+//监听7777接口 为github webhook 做相应，触发自动构建
 http.createServer((req, res) => {
-  handler(req, res, function(err) {
+  handler(req, res, function (err) {
     res.statusCode = 404
     res.end('no such location')
   })
 }).listen(7777)
- 
+
 handler.on('error', err => {
   console.error('Error:', err.message)
 })
- 
+
 handler.on('push', e => {
   try {
-  const s = spawn('sh', ['../node-server-auto-pull-git/build.sh'], {
-    cwd: `../${e.payload.repository.name}`
-  })
-  s.stdout.on('data', (data) => {
-    console.log(`${e.payload.repository.name}: ${data}`);
-  })
-  s.stderr.on('data', (data) => {
-    console.log(`${e.payload.repository.name}: ${data}`);
-  });
-  console.log(e.payload.repository.name, 'has rebuild');
-  } catch (e) {}
+    const s = spawn('sh', ['../node-server-auto-pull-git/build.sh'], {
+      cwd: `../${e.payload.repository.name}`
+    })
+    s.stdout.on('data', (data) => {
+      console.log(`${e.payload.repository.name}: ${data}`);
+    })
+    s.stderr.on('data', (data) => {
+      console.log(`${e.payload.repository.name}: ${data}`);
+    });
+    console.log(e.payload.repository.name, 'has rebuild');
+
+    app.use('/api/', createProxyMiddleware(proxyOption));
+
+    app.use(history()); 
+  
+    app.use(express.static(`../${e.payload.repository.name}/dist`))
+
+  } catch (e) { }
+
+
 })
 
 
-// const s = spawn('sh', ['../node-server-auto-pull-git/build.sh'], {
-//   cwd: `../qq_kandian_H5_11`
-// })
 
-// s.stdout.on('data', (data) => {
-//   console.log(`qq_kandian_H5_11: ${data}`);
-// })
-// s.stderr.on('data', (data) => {
-//   console.log(`qq_kandian_H5_11: ${data}`);
-// });
-// console.log("qq_kandian_H5_11", 'has rebuild');
+app.listen(8080, () => {
+  console.log(`App listening at port 8080`)
+})
--- build.sh
+++ build.sh
@@ -2,12 +2,10 @@ git reset --hard origin/master
 git clean -f
 git pull
 npm install -g cnpm --registry=https://registry.npm.taobao.org
-cnpm install --global http-server
+# cnpm install --global http-server
 cnpm install
 cnpm run build
 
-cd ../dist
-
-http-server ./dist -g -p 9999 --proxy http://116.62.197.100:1337/
+# http-server ./dist -g -p 9999 --proxy http://116.62.197.100:1337/
  
 echo 'build end'
